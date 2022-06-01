77bf485d8c3653ab5f5ce6a74d33821fb9bdd096
David Ricardo Pardo <david.pardo@aldeamo.com>
2022-06-01 10:28:02 -0500

    https services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiCountry: 'https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json',
-  apiServices: 'http://enviaservices.404namenotfound.live/api',
+  apiServices: 'https://enviaservices.404namenotfound.live/api',
   apiKeyMaps: 'AIzaSyDybihmidjkWncTMrLuqnInXXkNOiDDx14',
   firebaseConfig: {
     apiKey: "AIzaSyCM1Hi8zbT5UeO60u0TfE0fkDMZmtQxNYY",
